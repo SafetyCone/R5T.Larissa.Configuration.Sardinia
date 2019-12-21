1c826df347d78f57a4ad0920f29aec2ab95d6db5
MinexAutomation <minex.automation@gmail.com>
2019-12-21 13:55:04 -0800

    Implemented a configuration-based SVN executable file path provider.

---
+++ source/R5T.Larissa.Configuration.Sardinia/Code/Services/Implementations/ConfigurationBasedSvnExecutableFilePathProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.Options;
+
+
+namespace R5T.Larissa.Configuration
+{
+    public class ConfigurationBasedSvnExecutableFilePathProvider : ISvnExecutableFilePathProvider
+    {
+        private IOptions<SvnConfiguration> SvnConfiguration { get; }
+
+
+        public ConfigurationBasedSvnExecutableFilePathProvider(IOptions<SvnConfiguration> svnConfiguration)
+        {
+            this.SvnConfiguration = svnConfiguration;
+        }
+
+        public string GetSvnExecutableFilePath()
+        {
+            var svnConfiguration = this.SvnConfiguration.Value;
+
+            var svnExecutableFilePath = svnConfiguration.SvnExecutableFilePath;
+            return svnExecutableFilePath;
+        }
+    }
+}

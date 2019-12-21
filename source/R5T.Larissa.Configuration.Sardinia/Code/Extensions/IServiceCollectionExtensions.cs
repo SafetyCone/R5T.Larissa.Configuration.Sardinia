488127ea039bc2d0de7d30d3642bb02d4057b66a
MinexAutomation <minex.automation@gmail.com>
2019-12-21 13:54:36 -0800

    Added service collection extension to add SVN configuration.

---
+++ source/R5T.Larissa.Configuration.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Sardinia;
+
+using RawSvnConfiguration = R5T.Larissa.Configuration.Raw.SvnConfiguration;
+
+
+namespace R5T.Larissa.Configuration.Sardinia
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSvnConfiguration(this IServiceCollection services)
+        {
+            services
+                .Configure<RawSvnConfiguration>()
+                .ConfigureOptions<SvnConfigurationConfigureOptions>()
+                ;
+
+            return services;
+        }
+    }
+}
